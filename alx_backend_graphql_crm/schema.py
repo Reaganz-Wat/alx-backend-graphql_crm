524031f6d03cdcecb0c63b020b8a4f76a7eb7779
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-08-17 23:03:31 +0300

    remove schema line from schema.py file

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -4,8 +4,6 @@ from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
-schema = graphene.Schema(query=Query)
-
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
